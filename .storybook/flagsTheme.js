78aa5cd9c86f7a208cb39508d1d63cd50f249c32
Adam Sokołowski <adam.sokolowski@gmail.com>
2020-08-15 21:02:17 +0200

    feat(storybook): update storybook, add theme, update dependencies

---
+++ .storybook/flagsTheme.js
@@ -0,0 +1,32 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'dark',
+
+  // UI
+  appBg: 'white',
+  appContentBg: 'white',
+  appBorderColor: 'white',
+  appBorderRadius: 0,
+
+  // Typography
+  fontBase: '"Arial", sans-serif',
+  fontCode: 'monospace',
+
+  // Text colors
+  textColor: 'black',
+  textInverseColor: 'rgba(255,255,255,0.9)',
+
+  // Toolbar default and active colors
+  barTextColor: 'white',
+  barSelectedColor: 'black',
+  barBg: 'black',
+
+  // Form colors
+  inputBg: 'white',
+  inputBorder: 'black',
+  inputTextColor: 'black',
+  inputBorderRadius: 4,
+
+  brandTitle: 'Countries flags',
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import flagsTheme from './flagsTheme';
+
+addons.setConfig({
+  theme: flagsTheme,
+});

3afae28ad4e4594ddf0690020ff459a9d2465fbe
Adam Sokołowski <adam.sokolowski@europcar.com>
2022-12-24 17:52:35 +0100

    chore(all): splits further files

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -10,12 +10,11 @@ const sizes = {
   ExtraLarge: 'xl',
 };
 
-const size = select('Size', sizes, 'm');
+const size = select('Size', sizes, 'l');
 
-const getTemplate = (letter) => `
+const getTemplate = () => `
   <div class="padding">
     ${COUNTRY_CODES
-    .filter(country => country.name.startsWith(letter))
     .map(country => `
         <div class="wrapper">
           <div class="container">
@@ -34,31 +33,7 @@ const getTemplate = (letter) => `
   }
 </div>`;
 
-storiesOf('Countries', module)
-  .add('A', () => getTemplate('A'))
-  .add('B', () => getTemplate('B'))
-  .add('C', () => getTemplate('C'))
-  .add('D', () => getTemplate('D'))
-  .add('E', () => getTemplate('E'))
-  .add('F', () => getTemplate('F'))
-  .add('G', () => getTemplate('G'))
-  .add('H', () => getTemplate('H'))
-  .add('I', () => getTemplate('I'))
-  .add('J', () => getTemplate('J'))
-  .add('K', () => getTemplate('K'))
-  .add('L', () => getTemplate('L'))
-  .add('M', () => getTemplate('M'))
-  .add('N', () => getTemplate('N'))
-  .add('O', () => getTemplate('O'))
-  .add('P', () => getTemplate('P'))
-  .add('Q', () => getTemplate('Q'))
-  .add('R', () => getTemplate('R'))
-  .add('S', () => getTemplate('S'))
-  .add('T', () => getTemplate('T'))
-  .add('U', () => getTemplate('U'))
-  .add('V', () => getTemplate('V'))
-  .add('Y', () => getTemplate('Y'))
-  .add('Z', () => getTemplate('Z'));
+storiesOf('Countries', module).add('List', () => getTemplate());
 
 export default {
   title: 'Adjust flag size',
